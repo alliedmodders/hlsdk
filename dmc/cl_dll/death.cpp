f4f493fab33bbcd7f588d5aa3a2f574a725f6c52
Wei Mingzhi <weimingzhi@baidu.com>
2018-08-27 11:03:16 +0800

    fix possible buffer overflow

--- dmc/cl_dll/death.cpp
+++ dmc/cl_dll/death.cpp
@@ -179,7 +179,7 @@ int CHudDeathNotice :: MsgFunc_DeathMsg( const char *pszName, int iSize, void *p
 	int killer = READ_BYTE();
 	int victim = READ_BYTE();
 
-	char killedwith[32];
+	char killedwith[64];
 	strcpy( killedwith, "d_" );
 	strncat( killedwith, READ_STRING(), 32 );
 
